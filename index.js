82032ba0399969893984defa7bf732d9b976dd54
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-02-18 08:43:59 -0600

    removed questions array

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
